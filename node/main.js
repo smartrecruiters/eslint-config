ae947cf4d161e23d74bd3a9a8db99cd2f40fb6cb
Szymon Kiebzak <s.kiebzak@smartrecruiters.com>
2019-09-02 12:28:49 +0200

    Remove prefer-object-spread from node8 config (#10)

--- node/main.js
+++ node/main.js
@@ -1,18 +1,1 @@
-module.exports = {
-    env: {
-        node: true,
-        es6: true
-    },
-    parserOptions: {
-        ecmaVersion: 2017
-    },
-    extends: [
-        'eslint:recommended',
-        '../rules/best-practices.yaml',
-        '../rules/ecmascript-6.yaml',
-        '../rules/node_js-and-commonjs.yaml',
-        '../rules/possible-errors.yaml',
-        '../rules/stylistic-issues.yaml',
-        '../rules/variables.yaml'
-    ]
-}
+module.exports.extends = '../config/nodejs8.yaml'
